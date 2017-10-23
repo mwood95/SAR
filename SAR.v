10a36cfd73b0519406621b99e748fcb5b668d998
mikewood95 <mikewood95@gmail.com>
2017-10-22 20:26:24 -0700

    Added comments to SAR.v and adjusted code formatting.

--- SAR.v
+++ SAR.v
@@ -1,19 +1,30 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+// Developer: Michael Wood
+// Date: 10/22/2017
+// Description: This module implements a 4-bit Successive Approximation Register
+// (SAR). This model verifies the high level functionality of the block. The
+// ultimate design will result in an IC implemetation of a SAR ADC chip.
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
 module SAR(input in, input clock, input rst, output reg [3:0] out, state, next_state, output reg valid);
 
-  
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+// State Description using ONE-HOT encoding method
+///////////////////////////////////////////////////////////////////////////////////////////////////// 
   localparam B3     = 4'b0001;
   localparam B2     = 4'b0010;
   localparam B1     = 4'b0100;
   localparam B0     = 4'b1000;
   localparam DONE   = 4'b0000; 
-  
+ 
+ // Bit set values for SAR state machine 
   localparam B3SET  = 4'b1000;
   localparam B2SET  = 3'b100;
   localparam B1SET  = 2'b10;
   localparam B0SET  = 1'b1;
   
 
-  
+ // Start State Machine  
   always @ (posedge clock or posedge rst)
     begin 
       if(rst)
@@ -26,6 +37,8 @@ module SAR(input in, input clock, input rst, output reg [3:0] out, state, next_s
         state <= next_state;
       
     end
+
+ // Set ouput in each state
   always@(state)
     begin
       case(state)
@@ -46,7 +59,7 @@ module SAR(input in, input clock, input rst, output reg [3:0] out, state, next_s
                 out[3] <= 0;
               end
             out[2:0] <= B2SET;
-            next_state <= 4'b0100;
+            next_state <= B1;
           end
         
         B1:
@@ -88,6 +101,7 @@ module SAR(input in, input clock, input rst, output reg [3:0] out, state, next_s
                 out[0] <= 0;
               end
             valid <= 1;
+            next_state <= B3;
           end
       endcase
     end
